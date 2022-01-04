bfa226ae07c1837d9a5ef3d5b7b5f5484a7e36f9
Rob Campbell <raacampbell@users.noreply.github.com>
2022-01-04 13:58:09 +0000

    merge from dev

--- btpytools/compressRawData.py
+++ btpytools/compressRawData.py
@@ -14,22 +14,64 @@ asks for confirmation before continuing. If your ssh session breaks off
 for some reason, then compression will fail. tmux is therefore recomended
 in this situation.
 
+
+Runs much faster with parallel bzip (lbzip2) installed. If this is missing, the tool reverts
+to regular gzip.
+
 """
 
 import os
+from datetime import datetime
 import time
 from btpytools import tools, recipe
+from glob import glob
+
+def main(simulate=False):
+
+    if simulate:
+        print('\n\nRUNNING IN SIMULATE MODE\n')
 
 
-def main():
     if tools.has_raw_data() == False:
         print("No rawData folder found in", os.getcwd())
         exit()
 
 
-    # Generate the compressed file name from the sample name
-    compressedRawDataName = recipe.sample_id() + "_rawData.tar.bz"
-    print("Found rawData directory for sample " + recipe.sample_id())
+    # The sample ID is used to name the raw data directory. We get this information from
+    # the recipe file. We look for this in the current directory and, failing that, in the
+    # cropped data directory. If neither exists, we use the current directory name instead. 
+
+    # If there is no recipe file in the current directory, look for one in the cropped data dir. 
+    # if also that is missing, we use as the 
+
+    copy_files_from_uncropped = False # Do we need to temporarily copy settings files out of the uncropped data dir?
+
+    if tools.has_recipe_file():
+        sample_name = recipe.sample_id()
+    elif tools.has_uncropped_stitched_images():
+        # Can we get sample ID from a recipe file in the uncropped stacks directory?
+        uncropped_dir = glob(tools._uncropped_wildcard)[0]
+        if tools.has_recipe_file(uncropped_dir):
+            # We get the sample ID from the uncropped data directory, so we also want to later
+            # copy other settings files temporarily into the current directory so they
+            # get archived
+            print('Getting sample information from meta-data files in uncropped data directory')
+            copy_files_from_uncropped = True
+            sample_name = recipe.sample_id(uncropped_dir)
+        else:
+            # We find no recipe file anywhere but there were were raw data. Let's, therefore,
+            # use the current working directory and the date to build the sample name
+            print('No meta data files. Making up a sample ID from directory name.')
+            cwd = os.getcwd()
+            sample_name = cwd.split(cwd[0])[-1]
+
+            now = datetime.now()
+            sample_name  = sample_name + now.strftime('_%Y%m%d')
+
+
+
+
+    print("Found rawData directory for sample " + sample_name)
 
 
     # Begin compression, but warn if we aren't in a tmux session if this is an SSH connection
@@ -39,11 +81,47 @@ def main():
             exit()
 
 
+    # We will also copy metadata
+    meta_data_file_names = 'scanSettings.mat *.yml *.txt *.ini' # wildcards for files other than raw data that we will compress 
+
+
+    # Check whether we have lbzip2 and use it if so
+    out = os.system('lbzip2 -h > /dev/null')
+    has_lbzip2 = out==0
+
+    if has_lbzip2:
+        tar_switches = '-I lbzip2 -cvf'
+        compressed_rawdata_name = sample_name + "_rawData.tar.bz"
+
+    else:
+        tar_switches = '-zcvf'
+        compressed_rawdata_name = sample_name + "_rawData.tar.gz"
+        print('\n\n** WARNING -- parallel compression tool lbzip2 not found. Using much slower single-threaded gzip.\n\n')
+        time.sleep(3)
+
     # Start compressing with parallel bzip
-    cmd = 'tar -I lbzip2 -cvf ' + compressedRawDataName + ' scanSettings.mat *.yml *.txt *.ini ./rawData'
-    print("\nRunning compression command: " + cmd + "\n")
+    compress_cmd = 'tar %s %s %s ./rawData ' % (tar_switches, compressed_rawdata_name, meta_data_file_names)
+
+    print("\nRunning compression command: " + compress_cmd + "\n")
     time.sleep(1)
-    os.system(cmd)
+
+    if not simulate:
+        # If necessary we copy the meta-data files temporaily out of the uncropped stacks folder
+        if copy_files_from_uncropped:
+            print('Temporarily moving meta-data from uncropped directory into main directory')
+            for these_files in meta_data_file_names.split(' '):
+                os.system('cp %s/%s ./' % (uncropped_dir,these_files))
+
+        # Run the compression command
+        os.system(compress_cmd)
+
+        # If needed, remove the files that we added. The following is slightly dangerous, but if normal
+        # usage there should be no problem with the wild card delete. 
+
+        if copy_files_from_uncropped:
+            print('\nTidying up temporarily copied files')
+            os.system('rm -f %s' % meta_data_file_names)
+
 
 
 if __name__ == "__main__":
--- btpytools/tools.py
+++ btpytools/tools.py
@@ -4,12 +4,18 @@ General purpose tools for handling BakingTray data directories
 
 import sys
 import os
+import re
 from glob import glob
 
 
-"""
-The following are related to BakingTray data directories
-"""
+## Define variables that will be common across functions
+_stitchedImageDir = 'stitchedImages_*'
+_downsampledDir = 'downsampled_stacks'
+_downsampledStackSubDir = '*_micron' #Sub-directories in _downsampledDir
+_downsampledStackLogFile = 'ds_*.txt' #Wildcard for downsampled stacks
+_uncropped_wildcard = './Uncropped*_DELETE_ME_DELETE_ME'
+_recipe_wildcard = 'recipe*.yml'
+
 
 def has_raw_data(tPath=''):
     """ Check if current directory (or that defined by tPath) contains a rawData directory.
@@ -31,7 +37,7 @@ def has_recipe_file(tPath=''):
     """ Check if current directory (or that defined by tPath) contains a recipe file.
         Returns True if present, False if absent
     """
-    tPath = os.path.join(tPath,'recipe*.yml')
+    tPath = os.path.join(tPath,_recipe_wildcard)
     return file_glob_exist(tPath)
 
 
@@ -47,7 +53,7 @@ def has_stitched_images_directory(tPath=''):
     """ Check if current directory (or that defined by tPath) contains a stitched image directory
         Returns True if present, False if absent
     """
-    tPath = os.path.join(tPath,'./stitchedImages_*')
+    tPath = os.path.join(tPath,_stitchedImageDir)
     return file_glob_exist(tPath)
 
 
@@ -60,15 +66,15 @@ def has_stitched_stacks(tPath=''):
 
 
 def has_downsampled_stacks(tPath=''):
-    """ Check if current directory (or that defined by tPath) contains a downsampleStacks* directory
-        Returns True if present, False if absent
+    """ Check if current directory (or that defined by tPath) contains a downsampled_stacks directory
+        Returns True if present, False if absent.
     """
-    tPath = os.path.join(tPath,'./downsampled_stacks')
+    tPath = os.path.join(tPath,_downsampledDir)
     if not file_glob_exist(tPath):
         return False
-    else:
-        tPath = os.path.join(tPath,'*_micron')
-        print(tPath)
+
+    if not file_glob_exist( os.path.join(tPath,_downsampledStackSubDir) ):
+        return False
 
     return file_glob_exist(tPath)
 
@@ -77,7 +83,7 @@ def has_uncropped_stitched_images(tPath=''):
     """ Check if current directory (or that defined by tPath) contains a stitched image directory
         Returns True if present, False if absent
     """
-    tPath = os.path.join(tPath,'./Uncropped*_DELETE_ME_DELETE_ME')
+    tPath = os.path.join(tPath,_uncropped_wildcard)
     return file_glob_exist(tPath)
 
 
@@ -111,7 +117,77 @@ def contains_data_folders(dirToTest):
     return False
 
 
+def available_downsampled_volumes(tPath=''):
+    """ Check if current directory (or that defined by tPath) contains a downsampled_stacks directory
+        and returns a dictionary listing the available downsampled data. If none are present returns false. 
 
+    """
+    if not has_downsampled_stacks(tPath):
+        return False
+
+    # This, then, defines the downsampled stack directory and what the downsampled stacks are called
+    # The following line can have two layers of wildcards. e.g. downsampled_stacks/*_micron/ds_*.txt
+    tPath = os.path.join(tPath,_downsampledDir,_downsampledStackSubDir,_downsampledStackLogFile)
+    pathsToDownsampledStacks = glob(tPath)
+
+    if len(pathsToDownsampledStacks) < 1:
+        return False
+
+    out = []
+    for tPath in pathsToDownsampledStacks:
+        tLog=read_downsample_log_file(tPath)
+        out.append(tLog)
+
+    return out
+
+
+def read_downsample_log_file(pathToFile=''):
+    """ Parse StitchIt downsampled data file and return as a dictionary
+        pathToFile is the path to a dowsampled log file
+        returns False if no data are found in the defined path or multiple paths were supplied
+    """ 
+
+    if len(pathToFile) < 1:
+        return False
+    elif isinstance(pathToFile,list) and len(pathToFile)==1:
+        pathToFile = pathToFile[0]
+    elif isinstance(pathToFile,list) and len(pathToFile)>1:
+        return False
+
+    with open(pathToFile) as ff:
+        lines = ff.readlines()
+
+
+    ## Make a dictionary from the file contents
+    out = dict()
+
+    # Sample name
+    s=re.search('Downsampling (.*)', get_line_with_substr(lines,'Downsampling'))
+    out['samplename'] = s.group(1)
+
+    # Downsampled file name
+    s=re.search('downsample file name: (.*)', get_line_with_substr(lines,'downsample file name'))
+    out['path2stack'] = s.group(1)
+
+    # Acquisition date
+    s=re.search('Acquired on: (.*)', get_line_with_substr(lines,'Acquired'))
+    out['acqdate'] = s.group(1)
+
+    # Get the voxel size from the directory name
+    s=re.search('.*/(\d+)_micron/',pathToFile); 
+    out['voxelsize'] = int(s.group(1))
+
+    # Get the channel index from the file name
+    s=re.search('.*_ch(\d+)_chan_\d_',pathToFile); 
+    out['channelindex'] = int(s.group(1))
+
+    # Get the channel friendly name from the file name
+    s=re.search('.*_ch\d+_chan_\d_(.*)\.t',pathToFile); 
+    out['channelname'] = s.group(1)
+
+    return out
+
+ 
 
 """
 The following are very general purpose and not specific to BakingTray
@@ -172,3 +248,21 @@ def in_ssh_session():
         return True
     else:
         return False
+
+
+def get_line_with_substr(allLines,subStr):
+    """Return line from list of strings that contains a substring
+       False if nothing is found
+       Returns a list if multiple lines found
+    """
+
+    tLine = [myS for myS in allLines if subStr in myS]
+
+
+    if len(tLine)<1:
+        return False
+
+    if len(tLine)==1:
+        return tLine[0].rstrip()
+    else:
+        return tLine
