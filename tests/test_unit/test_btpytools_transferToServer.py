b38801f62647e32ffa64fff1f33013b30c465931
Rob Campbell <git@raacampbell.com>
2022-12-02 22:30:45 +0000

    overhaul tests
    Some tests were failing for weird reasons: the test data directory was wrong and missing stuff

--- tests/test_unit/test_btpytools_transferToServer.py
+++ tests/test_unit/test_btpytools_transferToServer.py
@@ -1,95 +1,17 @@
 """
-Unit tests for btpytools
-Run with "pytest tests/tests/basic.py" from project root
+Unit tests for btpytools.transferToServer
+Run with "pytest tests/test_unit/test_btpytools_transferToServer.py" from project root
+or simply run pytest to run all tests
 """
 
 
 import unittest
-import os
 from os.path import join
 from btpytools import transferToServer as tts
-from btpytools import tools
+from btpytools_test import btpytools_test
 
 
-DATA_DIR = join("tests", "data")
-VALID_SAMPLE_DIR1 = join(DATA_DIR, "valid_sample_directory_01")
-VALID_SAMPLE_DIR2 = join(DATA_DIR, "valid_sample_directory_02")
-CROPPED_ACQ_DIR1 = join(DATA_DIR, "contains_data_subfolders_01")
-CROPPED_ACQ_DIR2 = join(DATA_DIR, "contains_data_subfolders_02")
-
-
-class TestTools(unittest.TestCase):
-    """
-    Run tests associated with btpytools.tools
-    """
-
-    def test_valid_directory_is_valid(self):
-        """
-        Confirm if folder defined by VALID_SAMPLE_DIR1 is indeed valid
-        """
-        self.assertTrue(tools.is_data_folder(VALID_SAMPLE_DIR1))
-
-    def test_has_raw_data(self):
-        """
-        Confirm if VALID_SAMPLE_DIR1 contains a raw data folder
-        """
-        self.assertTrue(tools.has_raw_data(VALID_SAMPLE_DIR1))
-
-    def test_has_compressed_raw_data(self):
-        """
-        Confirm if VALID_SAMPLE_DIR1 contains a compressed data folder
-        """
-        self.assertTrue(tools.has_compressed_raw_data(VALID_SAMPLE_DIR1))
-
-    def test_has_recipe_file(self):
-        """
-        Confirm if VALID_SAMPLE_DIR1 contains a recipe file
-        """
-        self.assertTrue(tools.has_recipe_file(VALID_SAMPLE_DIR1))
-
-    def test_has_scan_settings(self):
-        """
-        Confirm if VALID_SAMPLE_DIR1 has a scan settings file
-        """
-        self.assertTrue(tools.has_scan_settings(VALID_SAMPLE_DIR1))
-
-    def test_has_stitched_images_directory(self):
-        """
-        Confirm if VALID_SAMPLE_DIR1 has a stitched images directory
-        """
-        self.assertTrue(tools.has_stitched_images_directory(VALID_SAMPLE_DIR1))
-
-    def test_uncropped_stitched_images(self):
-        """
-        Confirm if VALID_SAMPLE_DIR1 has an uncropped data directory
-        """
-        self.assertTrue(tools.has_uncropped_stitched_images(CROPPED_ACQ_DIR1))
-
-    def test_invalid_directories_are_invalid(self):
-        """
-        Confirm if that invalid directories are, indeed, invalid
-        """
-        invalid_dir = join(DATA_DIR, "invalid_data_dirs")
-
-        for t_path in os.listdir(invalid_dir):
-            self.assertFalse(tools.is_data_folder(join(invalid_dir, t_path)))
-
-    def test_contains_data_subfolders(self):
-        """
-        Confirm that folder contains sub-folders that are data folders
-        """
-        self.assertTrue(tools.contains_data_folders(CROPPED_ACQ_DIR1))
-
-    def test_contains_two_downsampled_dirs(self):
-        """
-        Confirm that the downsampled stacks directory in the valid sample has two downsampled
-        stacks
-        """
-        out = tools.available_downsampled_volumes(VALID_SAMPLE_DIR1, verbose=True)
-        self.assertTrue(len(out) == 4)
-
-
-class TestTransferToServer(unittest.TestCase):
+class TestTransferToServer(unittest.TestCase, btpytools_test):
     """
     Run tests associated with btpytools.transferToServer.
     Some functions within this module are hard to understand without the tests
@@ -99,7 +21,9 @@ class TestTransferToServer(unittest.TestCase):
         """
         Confirm if that that two data directories exist
         """
-        out = tts.check_directories(join(DATA_DIR, "dir_a"), join(DATA_DIR, "dir_b"))
+        out = tts.check_directories(
+            join(self.DATA_DIR, "dir_a"), join(self.DATA_DIR, "dir_b")
+        )
         self.assertTrue(out)
 
     def test_dir_list_contains_compressed_archive(self):
@@ -133,7 +57,10 @@ class TestTransferToServer(unittest.TestCase):
         """
         User specifies two cropped samples within a single acquisition
         """
-        t_dirs = [join(CROPPED_ACQ_DIR1, "dir1"), join(CROPPED_ACQ_DIR1, "dir1")]
+        t_dirs = [
+            join(self.CROPPED_ACQ_DIR1, "dir1"),
+            join(self.CROPPED_ACQ_DIR1, "dir1"),
+        ]
         self.assertTrue(tts.user_specified_cropped_directories_individually(t_dirs))
 
     def test_user_specifies_valid_directories(self):
@@ -141,12 +68,14 @@ class TestTransferToServer(unittest.TestCase):
         Should return False because this is not a cropped sample
         """
         self.assertFalse(
-            tts.user_specified_cropped_directories_individually(VALID_SAMPLE_DIR1)
+            tts.user_specified_cropped_directories_individually(
+                self.VALID_SAMPLE_DIR1
+            )
         )
 
         self.assertFalse(
             tts.user_specified_cropped_directories_individually(
-                [VALID_SAMPLE_DIR1, VALID_SAMPLE_DIR2]
+                [self.VALID_SAMPLE_DIR1, self.VALID_SAMPLE_DIR2]
             )
         )
 
@@ -155,16 +84,19 @@ class TestTransferToServer(unittest.TestCase):
         The user mixes indiviudally specified directories with "valid" samples directories.
         We should return true
         """
-        t_dirs = [join(CROPPED_ACQ_DIR1, "dir1"), join(CROPPED_ACQ_DIR1, "dir1")]
+        t_dirs = [
+            join(self.CROPPED_ACQ_DIR1, "dir1"),
+            join(self.CROPPED_ACQ_DIR1, "dir1"),
+        ]
 
         # With one valid sample directory at the list end
         t_1 = t_dirs
-        t_1.append(VALID_SAMPLE_DIR1)
+        t_1.append(self.VALID_SAMPLE_DIR1)
         self.assertTrue(tts.user_specified_cropped_directories_individually(t_1))
 
         # With one valid sample directory at the list start
         t_2 = t_dirs
-        t_2.insert(0, VALID_SAMPLE_DIR1)
+        t_2.insert(0, self.VALID_SAMPLE_DIR1)
         self.assertTrue(tts.user_specified_cropped_directories_individually(t_2))
 
     def test_user_specifies_one_directory_from_a_cropped_acq(self):
@@ -172,7 +104,7 @@ class TestTransferToServer(unittest.TestCase):
         Should return True because this is a cropped sample.
         """
         out = tts.user_specified_cropped_directories_individually(
-            join(CROPPED_ACQ_DIR1, "dir1")
+            join(self.CROPPED_ACQ_DIR1, "dir1")
         )
         self.assertTrue(out)
 
@@ -181,7 +113,10 @@ class TestTransferToServer(unittest.TestCase):
         Check whether, we correctly identify a list of source directories as being problematic
         for not having raw data being sent.
         """
-        f_1 = [join(CROPPED_ACQ_DIR1, "dir1"), join(CROPPED_ACQ_DIR1, "dir2")]
+        f_1 = [
+            join(self.CROPPED_ACQ_DIR1, "dir1"),
+            join(self.CROPPED_ACQ_DIR1, "dir2"),
+        ]
         self.assertTrue(tts.issue_warning_if_compressed_data_will_not_be_sent(f_1))
 
     def test_identify_acceptable_individually_specified_dirs(self):
@@ -191,16 +126,16 @@ class TestTransferToServer(unittest.TestCase):
 
         # Should pass because user supplies compressed archive
         t_1 = [
-            join(CROPPED_ACQ_DIR1, "dir1"),
-            join(CROPPED_ACQ_DIR1, "dir2"),
-            join(CROPPED_ACQ_DIR1, "compressed_rawData.tar.bz"),
+            join(self.CROPPED_ACQ_DIR1, "dir1"),
+            join(self.CROPPED_ACQ_DIR1, "dir2"),
+            join(self.CROPPED_ACQ_DIR1, "compressed_rawData.tar.bz"),
         ]
 
         self.assertFalse(tts.issue_warning_if_compressed_data_will_not_be_sent(t_1))
 
         # Should pass because specified dirs are in a directory with no compressed data
         crop_no_comp_dat = join(
-            DATA_DIR, "contains_data_subfolders_no_compress_data"
+            self.DATA_DIR, "contains_data_subfolders_no_compress_data"
         )
         t_2 = [join(crop_no_comp_dat, "dir1"), join(crop_no_comp_dat, "dir2")]
         self.assertFalse(tts.issue_warning_if_compressed_data_will_not_be_sent(t_2))
@@ -211,14 +146,18 @@ class TestTransferToServer(unittest.TestCase):
         """
 
         self.assertFalse(
-            tts.issue_warning_if_compressed_data_will_not_be_sent(VALID_SAMPLE_DIR1)
+            tts.issue_warning_if_compressed_data_will_not_be_sent(
+                self.VALID_SAMPLE_DIR1
+            )
         )
         self.assertFalse(
-            tts.issue_warning_if_compressed_data_will_not_be_sent(CROPPED_ACQ_DIR1)
+            tts.issue_warning_if_compressed_data_will_not_be_sent(
+                self.CROPPED_ACQ_DIR1
+            )
         )
 
         # Two directories containing cropped acquisitions should also pass
-        t_1 = [CROPPED_ACQ_DIR1, CROPPED_ACQ_DIR2]
+        t_1 = [self.CROPPED_ACQ_DIR1, self.CROPPED_ACQ_DIR2]
         self.assertFalse(tts.issue_warning_if_compressed_data_will_not_be_sent(t_1))
 
     def test_identify_peculiar_individually_specified_cases(self):
@@ -227,15 +166,15 @@ class TestTransferToServer(unittest.TestCase):
         Check these cases: when in doubt we just want to flag it.
         """
         f_1 = [
-            join(CROPPED_ACQ_DIR1, "dir1"),
-            join(CROPPED_ACQ_DIR1, "dir2"),
-            VALID_SAMPLE_DIR1,
+            join(self.CROPPED_ACQ_DIR1, "dir1"),
+            join(self.CROPPED_ACQ_DIR1, "dir2"),
+            self.VALID_SAMPLE_DIR1,
         ]
 
         f_2 = [
-            VALID_SAMPLE_DIR1,
-            join(CROPPED_ACQ_DIR1, "dir1"),
-            join(CROPPED_ACQ_DIR1, "dir2"),
+            self.VALID_SAMPLE_DIR1,
+            join(self.CROPPED_ACQ_DIR1, "dir1"),
+            join(self.CROPPED_ACQ_DIR1, "dir2"),
         ]
         self.assertTrue(tts.issue_warning_if_compressed_data_will_not_be_sent(f_1))
         self.assertTrue(
